c65e49d5e59cec2bde517497144d3af00d3c8e73
Makflo <mak.lorek1@gmail.com>
2020-04-22 11:06:28 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://makflo:PRdO4hpwpqlmAl6M@cluster0-5vlxa.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['KLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
